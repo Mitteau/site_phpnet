1f78a3ae6c9db6539575027fb4fa976fc7c1549f
Mitteau <perso@jcmitteau.net>
2017-06-17 09:54:49 +0200

    Votre branche est en retard sur 'origin/index' de 1 commit, et peut être mise à jour en avance rapide.    (utilisez "git pull" pour mettre à jour votre branche locale)
     Modifications qui seront validées :
    	nouveau fichier : commun/Zhong.png
    	nouveau fichier : commun/cadenas.png
    	nouveau fichier : commun/clavier.gif
    	nouveau fichier : commun/culdelampe.gif
    	nouveau fichier : commun/fdroite.png
    	nouveau fichier : commun/feed-icon-16x16.png
    	nouveau fichier : commun/fgauche.png
    	nouveau fichier : commun/new.gif
    	nouveau fichier : commun/stour_p.jpg
    	modifié :         css/design2.css
    	modifié :         inclus/contenu.php
    	supprimé :        inclus/forum.png
    	nouveau fichier : inclus/site.gif
    Finalisation de la nouvelle page index

--- inclus/contenu.php
+++ inclus/contenu.php
@@ -3,8 +3,9 @@
 </p>');
 ?>
 <div id="pave" style="float:center">
-<a href="http://www.jcmitteau.net" target="_blank"><img src="../inclus_pepita/Le Canon 2014©DM (66)_resultat.JPG" width="213" height="160" border="0" alt="pepita 3" name="_pepita3"title="Je vends mon monotype d'Arcachon"/></a>
-B
-<a href="http://www.jcmitteau.net/FORUM"><img src="forum.gif" title="Échanger" alt="pavé"></a>
+<a href="http://www.jcmitteau.net" target="_blank" style="display:inline"><img src="../inclus_pepita/Le Canon 2014©DM (66)_resultat.JPG" height="170" border="0" alt="pepita" name="_pepita3"title="Je vends mon monotype d'Arcachon"/></a>
+<a href="http://www.jcmitteau.net" style="display:inline"><img src="inclus/site.gif" title="Consulter" alt="site" height="170" border="0"></a>
+<a href="http://www.jcmitteau.net/FORUM" style="display:inline"><img src="inclus/forum.gif" title="Dialoguer" alt="forum" height="170" border="0"></a>
 
 </div>
+
